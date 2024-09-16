58d8970b5aa27e50da584959f39bbc64b7d9b8db
torano <fastrivertiger@gmail.com>
2024-09-16 19:00:48 +0900

    fix: resolve the fatal error when using open/save file dialog in a shipping package by replacing them with functions in EasyFileDialog

--- Source/HelpWriter/Public/HWFunctionLibrary.h
+++ Source/HelpWriter/Public/HWFunctionLibrary.h
@@ -25,56 +25,6 @@ class HELPWRITER_API UHWFunctionLibrary : public UBlueprintFunctionLibrary
 	GENERATED_BODY()
 
     ////////////////////
-    ///// File Dialog
-
-public:
-    /**
-     * Opens the "open file" dialog for the platform
-     *
-     * @param OutputPin		            Pin for blueprint nodes.
-     * @param OutFilePath				The absolute file paths that were selected in the dialog
-     * @param DialogTitle				The text for the title of the dialog window
-     * @param DefaultPath				The path where the file dialog will open initially
-     * @param DefaultFile				The file that the dialog will select initially
-     * @param FileType					The type filters to show in the dialog. This string should be a "|" delimited list of (Description|Extensionlist) pairs. Extensionlists are ";" delimited.
-     * @param IsMultiple				Determins if multiple file selections are allowed
-     */
-    UFUNCTION(BlueprintCallable, meta = (ExpandEnumAsExecs = "OutputPin"), Category = "HWFunctionLibrary|FileDialog")
-    static void OpenFileDialog(
-        EDialogResult& OutputPin,
-        TArray<FString>& OutFilePath,
-        const FString& DialogTitle = TEXT("Open File Dialog"),
-        const FString& DefaultPath = TEXT(""),
-        const FString& DefaultFile = TEXT(""),
-        const FString& FileType = TEXT("All (*)|*.*"),
-        bool IsMultiple = false
-    );
-
-    /**
-     * Opens the "save file" dialog for the platform
-     *
-     * @param OutputPin		            Pin for blueprint nodes.
-     * @param OutFilePath				The absolute file path to save in the dialog
-     * @param DialogTitle				The text for the title of the dialog window
-     * @param DefaultPath				The path where the file dialog will open initially
-     * @param DefaultFile				The file that the dialog will select initially
-     * @param FileType					The type filters to show in the dialog. This string should be a "|" delimited list of (Description|Extensionlist) pairs. Extensionlists are ";" delimited.
-     */
-    UFUNCTION(BlueprintCallable, meta = (ExpandEnumAsExecs = "OutputPin"), Category = "HWFunctionLibrary|FileDialog")
-    static void SaveFileDialog(
-        EDialogResult& OutputPin,
-        FString& OutFilePath, 
-        const FString& DialogTitle = TEXT("Open File Dialog"),
-        const FString& DefaultPath = TEXT(""),
-        const FString& DefaultFile = TEXT(""),
-        const FString& FileType = TEXT("All (*)|*.*")
-    );
-
-protected:
-    // Window handle getter
-    static void* GetWindowHandle();
-
-    ////////////////////
     ///// Importer/Exporter
 public:
     UFUNCTION(BlueprintCallable, Category = "HWFunctionLibrary|Exporter")
